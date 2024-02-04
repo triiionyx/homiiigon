2f69c3dda8adbc6d560a20de9a475c4c17a981e0
Dap Moderator <123246207+thebanon@users.noreply.github.com>
2024-02-03 23:59:11 -0500

    Update INDEX files

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 window.onload = () => {
+    var body = document.body;
+    var screen = body.querySelector('screen');
+    var pixel = document.createElement('pixel');
     
+    var x = 0;
+    const cols = 10;
+    const rows = 10;
+    do {
+        screen.insertAdjacentHTML('beforeend', pixel.outerHTML)    
+        x++;
+    } while(x < cols * rows);
 }
